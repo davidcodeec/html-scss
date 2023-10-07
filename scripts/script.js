373eab67584b6c672bcb4a9967633d049b13385c
David <david.obamaja@utb.ecutbildning.se>
2023-10-07 11:59:30 +0200

    Changes in index.html and css

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".main-menu");
+    const icon = document.querySelector(".menu-bars");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
